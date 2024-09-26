faf8850369d3f6d0e525b5e79a8f343ef5e6c6cb
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-26 08:20:35 -0400

    Add project files.

---
+++ Topic - 7 Lists/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic___7_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
